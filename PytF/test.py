d59ae5829b2e5e8b802628d2506213286650a3e7
Iqbal, Mirza Jhanzaib <mirza.jhanzaib.iqbal@accenture.com>
2021-04-14 15:26:08 +0200

    test

---
+++ PytF/test.py
@@ -0,0 +1,13 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+from pyspark.sql.types import *
+from pyspark.rdd import reduce
+import time
+import datetime
+import logging
+from pyspark.sql import SQLContext
